f9d721b99e2a6d2cce666506416ff748ced7ad8f
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-10-23 11:21:27 -0700

    updated submit method in SourdoughFormBuilder

--- app/builders/sourdough_form_builder.rb
+++ app/builders/sourdough_form_builder.rb
@@ -1,17 +1,21 @@
 class SourdoughFormBuilder < ActionView::Helpers::FormBuilder
   def email_field(method, options={})
-    super(method, options.reverse_merge(class: "sd-email-field"))
+    super(method, options.reverse_merge(class: "rounded-md px-2 py-1"))
   end
 
   def password_field(method, options={})
-    super(method, options.reverse_merge(class: "sd-password-field"))
+    super(method, options.reverse_merge(class: "rounded-md px-2 py-1"))
   end
 
   def check_box(method, options={})
-    super(method, options.reverse_merge(class: "sd-check-box"))
+    super(method, options.reverse_merge(class: "rounded-md"))
   end
 
   def label(method, options={})
-    super(method, options.reverse_merge(class: "sd-label"))
+    super(method, options.reverse_merge(class: "italic"))
+  end
+  
+  def submit(method, options={})
+    super(method, options.reverse_merge(class: "bg-rose-500 text-rose-50 py-2 px-4 rounded-md cursor-pointer hover:bg-rose-600"))
   end
 end
