b2b33b3be2c7eb9783c5c2fddcdcbb582a088160
Adrian Valenzuela <adrianvalenz.web@gmail.com>
2022-10-26 02:56:51 -0700

    devise user sign out spec passes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get :dashboard, to: "dashboard#show"
   devise_for :users
-  
+
+  get 'home/show', to: "home#show"
   root "home#show"
 end
--- spec/system/user_sign_up_flow_spec.rb
+++ spec/system/user_sign_up_flow_spec.rb
@@ -5,33 +5,27 @@ RSpec.describe "Devise User", :type => :system do
   it "logs in successfully" do
     user = FactoryBot.create(:user, email: "adrian@example.com", password: "secret")
     user.confirm
-
     visit new_user_session_path
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
     click_button "Log in"
-    
     expect(page).to have_text(I18n.t("devise.sessions.signed_in"))
   end
 
   it "enters invalid credentials" do
     user = FactoryBot.create(:user, email: "adrian@example.com", password: "secret")
-    
     visit new_user_session_path
-
     fill_in "Email", with: user.email
     fill_in "Password", with: "notsecret"
     click_button "Log in"
-
     expect(page).to have_text(I18n.t("devise.failure.invalid", authentication_keys: "Email"))
   end
 
   it "logs out" do
-    user = FactoryBot.create(:user, :email: "adrian@example.com", password: "secret")
-
+    login_as(FactoryBot.create(:user, confirmed_at: Time.now))
     visit root_path
-    click_link "sign-out-link"
-
-    # expect(page).to have_text(I18n.t("devise.x.x"))
+    click_on "main-nav-menu-button"
+    click_on "sign-out-link"
+    expect(page).not_to have_link('sign-out-link')
   end
 end
